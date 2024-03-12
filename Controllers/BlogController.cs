732424e7d9a657be75cf9986eb81f5cc32049638
JayvonK <jaykhuth@gmail.com>
2024-03-12 15:45:58 -0700

    first commit

---
+++ Controllers/BlogController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace blockBackend.Controllers;
+
+    [ApiController]
+    [Route("[controller]")]
+    public class BlogController : ControllerBase
+    {
+        
+    }
